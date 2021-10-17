d79730f73d0ce1a7dae130777d2d68b33f3bfd38
Kristo Markov <markovkristo@gmail.com>
2021-10-18 00:36:09 +0300

    Lahendasin ära H3

--- Projekt.R
+++ Projekt.R
@@ -15,17 +15,37 @@ df <- subset(df, select = -c(X))
 # H1: Disney+ edastab kõige vähem täiskasvanutele mõeldud sisuga filme.
 
 # Eraldan vajalikud andmed, et kontrollida seda hüpoteesi. Jätan alles ainult read, kus filmi vanusepiirang on 18+
-valik <- df %>% select(Age, Netflix, Hulu, Prime.Video) %>% filter(Age == "18+")
+valik1 <- df %>% select(Age, Netflix, Hulu, Prime.Video) %>% filter(Age == "18+")
 
 # Loen kokku, mitu täisealistele mõeldud filmi igal firmal on.
-netflix <- valik %>% filter(Netflix == 1) %>% count(Netflix)
-hulu <- valik %>% filter(Hulu == 1) %>% count(Hulu)
-amazon <- valik %>% filter(Prime.Video == 1) %>% count(Prime.Video)
+netflix1 <- valik1 %>% filter(Netflix == 1) %>% count(Netflix)
+hulu1 <- valik1 %>% filter(Hulu == 1) %>% count(Hulu)
+amazon1 <- valik1 %>% filter(Prime.Video == 1) %>% count(Prime.Video)
 
 # Moodustan uue data frame saadud andmetest, et oleks lihtsam joonist teha.
 firmad <- c("Netflix", "Hulu", "Prime Video")
-sagedus <- c(netflix$n, hulu$n, amazon$n)
+sagedus <- c(netflix1$n, hulu1$n, amazon1$n)
 taiskasvanuteleMoeldud <- data.frame(firmad, sagedus)
 
 # Joonistan tulemused välja
-ggplot(taiskasvanuteleMoeldud, aes(x = firmad, y = sagedus, fill = firmad)) + geom_col() 
+h1 <- ggplot(taiskasvanuteleMoeldud, aes(x = firmad, y = sagedus, fill = firmad)) + geom_col() 
+
+
+
+# H2: Netflixi filmivaliku keskmine rating on suurem kui muudel vaatamisplatvormidel.
+
+
+
+# H3: Netflixi filmivalik on kõige suurem.
+valik3 <- df %>% select(Age, Netflix, Hulu, Prime.Video) %>% filter(Age != "NA")
+netflix3 <- valik3 %>% filter(Netflix == 1) %>% count(Netflix)
+hulu3 <- valik3 %>% filter(Hulu == 1) %>% count(Hulu)
+amazon3 <- valik3 %>% filter(Prime.Video == 1) %>% count(Prime.Video)
+
+
+firmad3 <- c("Netflix", "Hulu", "Prime Video")
+sagedus3 <- c(netflix3$n, hulu3$n, amazon3$n)
+filmiValik <- data.frame(firmad, sagedus3)
+
+h3 <- ggplot(filmiValik, aes(x = firmad3, y = sagedus3, fill = firmad)) + geom_col() 
+
