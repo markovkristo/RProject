f114382e9b1972568bccdc8ff4c9832ae390670c
Kristo Markov <markovkristo@gmail.com>
2021-10-20 17:44:51 +0300

    Muutsin jooniseid ja korrastasin koodi.

--- Projekt.R
+++ Projekt.R
@@ -4,6 +4,8 @@ library(stringr)
 library(RColorBrewer)
 
 
+###############################################
+### Eeltöö ja andmete uurimine
 
 # Loen andmed sisse ja asendan kõik tühjad lahtrid NA-ga
 df <- read.csv("https://raw.githubusercontent.com/markovkristo/RProject/master/Filmid.csv", header = T, na.strings = c("","NA"))
@@ -31,15 +33,15 @@ naVaba <- na.omit(df)
 kirjeldused <- naVaba %>% mutate(keskmine_reiting = (IMDb + Rotten.Tomatoes) / 2)
 
 
-kirjeldused <- kirjeldused %>% summarise(suurim_keskmine_reiting = max(keskmine_reiting),
-                                     koigi_keskmine_reiting = mean(keskmine_reiting),
-                                     vaikseim_keskmine_reiting = min(keskmine_reiting),
-                                     pikim_film = max(Runtime),
-                                     keskmine_film = mean(Runtime),
-                                     luhim_film = min(Runtime))
+kirjeldused <- kirjeldused %>% summarise(suurim_keskmine_reiting = max(keskmine_reiting), # Mis on kõige suurem keskmine reiting filmile.
+                                     koigi_keskmine_reiting = mean(keskmine_reiting), # Mis on keskmine filmide reiting
+                                     vaikseim_keskmine_reiting = min(keskmine_reiting), # Mis on kõige madalam keskmine reitingfilmile.
+                                     pikim_film = max(Runtime), # Mis on kõige pikem film.
+                                     keskmine_film = mean(Runtime), # Keskmine filmi kestvus. 
+                                     luhim_film = min(Runtime)) # Mis on kõige lühem film.
 
-
-# H1: Disney+ edastab kõige vähem täiskasvanutele mõeldud sisuga filme.
+###############################################
+### H1: Disney+ edastab kõige vähem täiskasvanutele mõeldud sisuga filme.
 
 # Eraldan vajalikud andmed, et kontrollida seda hüpoteesi. Jätan alles ainult read, kus filmi vanusepiirang on 18+
 valik1 <- df %>% select(Age, Netflix, Hulu, Prime.Video, Disney.) %>% filter(Age == "18+")
@@ -57,26 +59,32 @@ filmiValik <- data.frame(firmad, sagedused1)
 
 
 # Joonistan tulemused välja
-h1 <- ggplot(filmiValik, aes(x = firmad, y = sagedused1,fill = firmad)) + geom_col() + 
+h1 <- ggplot(filmiValik, aes(x = reorder(firmad, -sagedused1), y = sagedused1,fill = firmad)) + geom_col() + 
   geom_text(aes(label=sagedused1), vjust=-0.3, size=3.5) +
-  labs(x = "Vaatamisplatvormid", y = "Filmide arvud", title = "18+ filmide arv erinevatel vaatamisplatvormidel. ") +
+  labs(x = "Vaatamisplatvormid", 
+       y = "Filmide arv", 
+       title = "18+ filmide arv erinevatel vaatamisplatvormidel",
+       fill = "Vaatamisplatvormid") +
+  theme(plot.title = element_text(hjust=0.5)) +
+  scale_y_continuous(limits = c(0, 1200),   
+                     breaks = seq(0, 1200, by = 200), 
+                     labels = paste0(seq(0, 1200,by = 200)))+
   scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
-  scale_fill_brewer(palette = "BuPu")
+  scale_fill_brewer(palette = "BuPu") 
 
 h1
 
 
-#h1 <- ggplot(taiskasvanuteleMoeldud, aes(x = firmad, y = sagedus, fill = firmad)) + geom_col()+ scale_fill_manual("Platvormid", values = c("Netflix"="red", "Hulu" = "green ", "Prime Video " = "orange", "Disney+ " = "skyblue"))
-#h1_pie <- ggplot(taiskasvanuteleMoeldud, aes(x=firmad, y=sagedus, fill=firmad)) + geom_bar(stat="identity", width=1) + coord_polar("y",start=0)
+###############################################
 
 
-
-# H2: Netflixi filmivaliku keskmine rating on suurem kui muudel vaatamisplatvormidel.
+###############################################
+### H2: Netflixi filmivaliku keskmine rating on suurem kui muudel vaatamisplatvormidel.
 # Eraldan vajalikud andmed, et kontrollida hupoteesi.
 valik2 <- df %>% select(IMDb, Rotten.Tomatoes, Netflix, Hulu, Prime.Video, Disney.)
 
 
-# Eraldan filmid platvormi j?rgi
+# Eraldan filmid platvormi jargi
 netflix2 <- valik2 %>% filter(Netflix == 1)
 hulu2 <- valik2 %>% filter(Hulu == 1)
 amazon2 <- valik2 %>% filter(Prime.Video == 1)
@@ -106,22 +114,39 @@ total_ratings <- c(Total_rating_netflix, Total_rating_hulu, Total_rating_amazon,
 
 rating <- data.frame(firmad2, ratingud2)
 total_ratings_df <- data.frame(firmad2_total, total_ratings )
-h2 <- ggplot(rating, aes(x= firmad2, y=ratingud2, fill=firmad2)) + geom_col() + 
-  labs(x = "Vaatamisplatvormid", y = "Reiting", title = "Vaatamisplatvormide IMDb ja Rotten tomatoes keskmised reitingud") +
-  geom_text(aes(label=ratingud2),position=position_dodge(width = 0.9),vjust=-0.25) + ylim(0,100) + scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
+
+#  Joonis kõigi filmide Rotten tomatoes-i ja IMDb reitingute keskmine (Rotten tomatoes ja IMDb eraldi) vaatamisplatvormi kohta.
+h2 <- ggplot(rating, aes(x= firmad2, y=ratingud2, fill=firmad2)) + geom_col() +
+  labs(x = "Vaatamisplatvormid", 
+       y = "Reiting", 
+       title = "Vaatamisplatvormide IMDb ja Rotten tomatoes keskmised reitingud",
+       fill = "Vaatamisplatvormid") +
+  theme(plot.title = element_text(hjust=0.5)) +
+  geom_text(aes(label=ratingud2),position=position_dodge(width = 0.9),vjust=-0.25) + 
+  ylim(0,100) + 
+  scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
   scale_fill_brewer(palette = "BuPu")
 
-
+#  Joonis kõigi filmide Rotten tomatoes-i ja IMDb reitingute keskmine ((Rotten tomato + IMDb) / 2) vaatamisplatvormi kohta.
 h2_total <- ggplot(total_ratings_df, aes(x = firmad2_total, y= total_ratings, fill=firmad2_total)) + geom_col() +
-  labs(x = "Vaatamisplatvormid", y = "Reiting", title = "Vaatamisplatvormide IMDb ja Rotten tomatoes keskmised reitingud") +
-  geom_text(aes(label=total_ratings),position=position_dodge(width = 0.9),vjust=-0.25) + ylim(0,100)+ scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
+  labs(x = "Vaatamisplatvormid", 
+       y = "Keskmine reiting", 
+       title = "Vaatamisplatvormide IMDb ja Rotten Tomatoes keskmised reitingud",
+       fill = "Vaatamisplatvormid") +
+  theme(plot.title = element_text(hjust=0.5)) + 
+  geom_text(aes(label=total_ratings),position=position_dodge(width = 0.9),vjust=-0.25) + 
+  ylim(0,100) + 
+  scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
   scale_fill_brewer(palette = "BuPu")
 
 h2
 h2_total
+###############################################
 
 
-# H3: Netflixi filmivalik on koige suurem.
+
+###############################################
+### H3: Netflixi filmivalik on koige suurem.
 valik3 <- df %>% select(Netflix, Hulu, Prime.Video, Disney.)
 valik3 <- na.omit(valik3)
 pikkFormaat <- melt(valik3, measure.vars = 1:4) %>% filter(value == 1) # Andmete teisendamine laiast formaadist pikka formaati.
@@ -140,57 +165,90 @@ filmideSagedus <- data.frame(firmad, firmadeSagedused)
 
 
 # Teen iga Vaatamisplatvormi filmidearvu kohta tulpdiagrammi.
-h3 <- ggplot(filmideSagedus, aes(x = firmad, y = firmadeSagedused, fill = firmad)) + geom_col() + 
-  geom_text(aes(label=sagedused3), vjust=-0.3, size=3.5) +
-  labs(x = "Vaatamisplatvormid", y = "Filmide arvud", title = "Filmide arv erinevatel vaatamisplatvormidel. ") + scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
+h3 <- ggplot(filmideSagedus, aes(x = reorder(firmad, -firmadeSagedused), y = firmadeSagedused, fill = firmad)) + geom_col() + 
+  geom_text(aes(label=firmadeSagedused), vjust=-0.3, size=3.5) +
+  labs(x = "Vaatamisplatvormid", 
+       y = "Filmide arv", 
+       title = "Filmide arv erinevatel vaatamisplatvormidel. ", 
+       fill = "Vaatamisplatvormid") +
+  theme(plot.title = element_text(hjust=0.5)) +
+  scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
   scale_fill_brewer(palette = "BuPu") +
   scale_y_continuous(limits = c(0, 4200),   
                      breaks = seq(0, 4200, by = 500), 
                      labels = paste0(seq(0, 4200,by=500)))
+
+
 h3
 
-
-sagedusedProtsentides <- round(firmadeSagedused/sum(firmadeSagedused)*100)
-ggplot(filmideSagedus, aes(x="", y=sagedusedProtsentides, fill=firmad)) +
+h3Pie <- ggplot(filmideSagedus, aes(x="", y=firmadeSagedused, fill=firmad)) +
   geom_bar(stat="identity", width=1, color="white") +
-  geom_text(aes(label=sagedusedProtsentides), hjust="inward") +
-  coord_polar("y", start=0) + 
-  labs(y = "Filmide protsent kõikidest filmidest", title = "Filmide arv erinevatel vaatamisplatvormidel.")+ scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
+  theme_classic() +
+  theme(plot.title = element_text(hjust=0.5),
+        axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank()) +
+  geom_text(aes(label = paste(round(firmadeSagedused / sum(firmadeSagedused) * 100, 1), "%")),
+            position = position_stack(vjust = 0.5)) +
+  coord_polar("y") + 
+  labs(x = NULL, 
+       y = NULL, 
+       title = "Filmide arv erinevatel vaatamisplatvormidel.",
+       fill = "Vaatamisplatvormid") + 
+  scale_colour_brewer(palette = "YlOrRd", direction = - 1) + 
   scale_fill_brewer(palette = "BuPu")
 
+h3Pie
 
 
-# Mis on kõige populaarsemad žanrid?
+###############################################
+
+
+
+###############################################
+### Mis on koige populaarsemad žanrid?
 Genres <- unlist(str_split(df$Genres, ",")) # Eraldan kõik žanrid koma koha pealt ning moodustan listi.
 zanriteSagedus <- data.frame(Genres)
-zanriteSagedus <- zanriteSagedus %>% count(Genres, sort = T) %>% arrange(desc(n))
+zanriteSagedus <- zanriteSagedus %>% count(Genres, sort = T) %>% filter(n > 1500) %>% arrange(desc(n))
 
-ggplot(data = zanriteSagedus, aes(Genres, n,fill=Genres)) + geom_bar(stat="identity") +
-  geom_text(aes(label=n), vjust=-0.3, size=3.5)  + labs(x = "Genres", y = "Count")
+ggplot(data = zanriteSagedus, aes(x = reorder(Genres, -n), n,fill=Genres)) + geom_bar(stat="identity") +
+  geom_text(aes(label=n), vjust=-0.3, size=3.5)  + 
+  labs(x = "Žanrid", 
+       y = "Filmide arv", 
+       title = "Top5 levinud filmižanrid vaatamisplatvormidel kokku",
+       fill = "Žanrid") + 
+  theme(plot.title = element_text(hjust=0.5)) +
+  scale_fill_brewer(palette = "BuPu")
+###############################################
 
 
-# Mis aastal tehtud filme on kõige rohkem nendel kanalitel.
-aastateEsinemine <- df %>% count(Year,sort = T)  # Loen aasta arvud kokku ning sorteerin need ära.
-#ggplot(data = aastateEsinemine, aes(Year, n)) + geom_bar() # Tulbana 
-ggplot(data = aastateEsinemine, aes(Year, n)) + geom_line() + 
-  labs(x = "Year", y = "Count", title = "Välja lastud filmide arv") +
+
+###############################################
+### Mis aastal tehtud filme on koige rohkem nendel kanalitel.
+filmideArvAastates <- df %>% count(Year,sort = T)  # Loen aasta arvud kokku ning sorteerin need ara.
+filmideArvAastates <- na.omit(filmideArvAastates)
+# Teen joondiagrammi, et naha uldiselt, kuidas on filmide valjalaskmine ajas muutunud. 
+ggplot(data = filmideArvAastates, aes(Year, n)) + geom_line() + 
+  labs(x = "Aasta", 
+       y = "Filmide arv", 
+       title = "Välja lastud filmide arv") +
+  theme(plot.title = element_text(hjust=0.5)) +
   scale_x_continuous(limits = c(1915, 2025),   
                      breaks = seq(1915, 2025, by = 10), 
                      labels = paste0(seq(1915, 2025,by=10))) +
   scale_y_continuous(limits = c(0, 1100),   
                      breaks = seq(0, 1100, by = 150), 
                      labels = paste0(seq(0, 1100,by=150)))
+###############################################
 
 
 
-# Uurin, mis filme näidatakse mitmelt vaatamisplatvormilt. 
+###############################################
+# Uurin, mis filme naidatakse mitmelt vaatamisplatvormilt. 
 filmideKorduvus <- df %>% select (Title, IMDb, Rotten.Tomatoes, Netflix, Hulu, Prime.Video, Disney.) %>% mutate(korduvus = Netflix + Hulu + Disney. + Prime.Video)  %>% count(korduvus, sort = T)
 
 
+###############################################
 
 
-h1
-h1_pie
-h2
-h2_total
-h3
+
